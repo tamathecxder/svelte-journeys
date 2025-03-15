cfcc8472cd925014ea578bc3029442f5a37b6eff
tamathecxder <yudistiraeka.pratama012@gmail.com>
2025-03-16 00:03:19 +0700

    (bindings) select

--- src/routes/bindings/input/+page.svelte
+++ src/routes/bindings/input/+page.svelte
@@ -3,6 +3,29 @@
     let a = $state(1);
 	let b = $state(2);
     let yes = $state(false);
+    let selected = $state();
+    let answer = $state('');
+
+    let questions = $state([
+		{
+			id: 1,
+			text: `Where did you go to school?`
+		},
+		{
+			id: 2,
+			text: `What is your mother's name?`
+		},
+		{
+			id: 3,
+			text: `What is another personal fact that an attacker could easily find with Google?`
+		}
+	]);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        alert(`Answered question ${selected.id} ${selected.text} with ${answer}`)
+    }
 </script>
 
 <!-- Input Text -->
@@ -29,3 +52,30 @@
 	Yes! Send me regular email spam = {yes ? 'Great' : 'Ok, No Problem!'}
 </label>
 
+<!-- Select -->
+<h2>Insecurity questions</h2>
+
+<form onsubmit={handleSubmit}>
+	<select
+		bind:value={selected}
+		onchange={() => (answer = '')}
+	>
+		{#each questions as question}
+			<option value={question}>
+				{question.text}
+			</option>
+		{/each}
+	</select>
+
+	<input bind:value={answer} />
+
+	<button disabled={!answer} type="submit">
+		Submit
+	</button>
+</form>
+
+<p>
+	selected question {selected
+		? selected.id
+		: '[waiting...]'}
+</p>
