d48d2cd90f1e05318fe7d79283348f67c796b337
tamathecxder <yudistiraeka.pratama012@gmail.com>
2025-03-15 23:54:55 +0700

    (bindings) numbered input

--- src/routes/bindings/input/+page.svelte
+++ src/routes/bindings/input/+page.svelte
@@ -1,7 +1,24 @@
 <script>
 	let name = $state('world');
+    let a = $state(1);
+	let b = $state(2);
 </script>
 
+<!-- Input Text -->
 <input bind:value={name} />
 
 <h1>Hello {name}!</h1>
+
+<!-- Input Number -->
+<label>
+	<input type="number" bind:value={a} min="0" max="10" />
+	<input type="range" bind:value={a} min="0" max="10" />
+</label>
+
+<label>
+	<input type="number" bind:value={b} min="0" max="10" />
+	<input type="range" bind:value={b} min="0" max="10" />
+</label>
+
+<p>{a} + {b} = {a + b}</p>
+
