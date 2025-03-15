4c0bd337bf5ffa53b993b4cb56a46b79a46f6101
tamathecxder <yudistiraeka.pratama012@gmail.com>
2025-03-15 16:27:17 +0700

    (events) component events

--- src/routes/events/capturing/+page.svelte
+++ src/routes/events/capturing/+page.svelte
@@ -1,3 +1,5 @@
 <div onkeydowncapture={(e) => alert(`<div> ${e.key}`)} role="presentation">
+    <a aria-label="component-events" href="/events/component-events">Navigate to capturing component events</a>
+    
 	<input onkeydowncapture={(e) => alert(`<input> ${e.key}`)} />
 </div>
